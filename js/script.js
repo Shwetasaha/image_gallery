7cada928eab882983c8d6062d6e4fa61464d3e25
Shwetasaha <shwetasaha47@gmail.com>
2018-09-23 12:51:37 +0530

    image_gallery
    Food across the world

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function()){
+  var galleryImage= $(".gallery").find("img").first();
+
+}
+
+var images=[
+ "images/9.png"
+ "images/13.png"
+ "images/14.png"
+]
+
+var i=0;
+var k;
+
+$(document).ready(function(){
+  $("#h").mouseover(function(){
+    $("#h").css("font-size",50);
+  });
+   $("#h").mouseout(function(){
+    $("#h").css("font-size",50);
+  });
+});
+
